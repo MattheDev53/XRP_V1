c4b6d81ef588be38d422683f38f7a3adfce9164d
MattheDev53 <110939089+MattheDev53@users.noreply.github.com>
2025-03-24 07:40:46 -0500

    Change Static Values to Constants
    These values are normally considered constants. If this is for teaching newer programmers that these should be constants, I'm alright with that. I was expecting to use the Reefscape code base, but this might be something neat too.

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,14 +11,9 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.xrp.XRPGyro;
 import edu.wpi.first.wpilibj.xrp.XRPMotor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
 
 public class Drivetrain extends SubsystemBase {
-  private static final double kGearRatio =
-      (30.0 / 14.0) * (28.0 / 16.0) * (36.0 / 9.0) * (26.0 / 8.0); // 48.75:1
-  private static final double kCountsPerMotorShaftRev = 12.0;
-  private static final double kCountsPerRevolution = kCountsPerMotorShaftRev * kGearRatio; // 585.0
-  private static final double kWheelDiameterInch = 2.3622; // 60 mm
-
   // The XRP has the left and right motors set to
   // channels 0 and 1 respectively
   private final XRPMotor m_leftMotor = new XRPMotor(0);
@@ -50,8 +45,8 @@ public class Drivetrain extends SubsystemBase {
     m_rightMotor.setInverted(true);
 
     // Use inches as unit for encoder distances
-    m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
-    m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
+    m_leftEncoder.setDistancePerPulse((Math.PI * DrivetrainConstants.kWheelDiameterInch) / DrivetrainConstants.kCountsPerRevolution);
+    m_rightEncoder.setDistancePerPulse((Math.PI * DrivetrainConstants.kWheelDiameterInch) / DrivetrainConstants.kCountsPerRevolution);
     resetEncoders();
   }
 
